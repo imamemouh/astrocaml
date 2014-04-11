fdfd9d12a52af0ce5f12c1bdee3815855e3a1c9b
Pierre Boudes <boudes@univ-paris13.fr>
2014-04-11 18:46:19 +0200

    La ramasseuse d'étoiles

--- astro.ml
+++ astro.ml
@@ -5,19 +5,29 @@ let perso = Images.hornGirl;;
 Graphics.open_graph " 350x350";;
 Graphics.set_color (Graphics.rgb 0 40 180);;
 Graphics.fill_rect 0 0 350 350;;
+Graphics.set_color (Graphics.rgb 0 128 255);;
+let f i =
+  Graphics.draw_segments [|
+      (50*i,0,50*i,350);
+      (0,50*i,350,50*i)
+     |];
+in
+List.iter f [1;2;3;4;5;6];;
+
 Graphics.remember_mode false;;
 
 let afficher img (i,j) =
-  Dessiner.dessiner_image img (j * 50) (300 - i * 50);;
+  Dessiner.dessiner_image img (!j * 50) (300 - !i * 50);;
 
-let etoiles = [(0,0);(6,6);(4,5);(2,2);(3,5)];;
+let etoiles =  [(ref 0,ref 0);(ref 6,ref 6);(ref 4, ref 5)
+                ;(ref 2, ref 2);(ref 3,ref 5)];;
 
 let perso_i, perso_j = ref 3, ref 3;;
 
 let afficher_decor () =
   Graphics.synchronize();;
 let afficher_perso() =
-  afficher perso (!perso_i,!perso_j);;
+  afficher perso (perso_i,perso_j);;
 let afficher_mobiles () =
   List.iter (afficher Images.star) etoiles;
   afficher_perso ();;
@@ -30,6 +40,14 @@ let deplacer direction =
     | S -> perso_i := !perso_i + 1
     | N -> perso_i := !perso_i - 1
   in
+  let ramasser_etoile (ri,rj) =
+    if (!ri,!rj) = (!perso_i,!perso_j) then
+      begin
+        ri := -1;
+        rj := 7;
+      end
+  in
+  List.iter ramasser_etoile etoiles;
   afficher_decor ();
   afficher_mobiles ();;
 
